f1f79ea48c5965f6b91256a9b9f1c92d75b9b5a6
hasanCoder66 <hasanashraf874@gmail.com>
2026-05-15 10:35:32 +0500

    how to upload image cloudinary using multer in express js

--- nodejs/code/file-uploading-multer/src/app.js
+++ nodejs/code/file-uploading-multer/src/app.js
@@ -8,12 +8,12 @@ import cloudinary from "./config/cloudinary.js"
 
 const storage = multer.diskStorage({
     destination : (req, file, cb) => {
-        cb(null, "./uploads")
+        cb(null, "./src/uploads")
     },
     filename : (req, file, cb) => {
-        console.log(file,"file -->")
+        // console.log(file,"file -->")
 
-        console.log(req, "req -->")
+        // console.log(req, "req -->")
         cb(null, `${Date.now()}-${file.originalname}`)
     },
 
@@ -22,12 +22,58 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage})
 
+// const upload = multer({ dest: './src/uploads' })
+
 export const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
+
+app.post("/profile", upload.single("avatar")  , async (req, res) =>{
+
+    try {
+
+        const file = req.file
+        const {title, description} = req.body;
+
+        if (!file) {
+                return res.status(400).json({
+                    status: false,
+                    message: "Profile is required"
+                })
+            }
+
+            
+               const cloudResponse = await cloudinary.uploader.upload(
+                file.path,
+                {
+                    folder: "users"
+                }
+            )
+
+          
+         let post =   await Post.create({
+                title: title,
+                description : description,
+                postImage : cloudResponse.secure_url
+            })
+            
+            
+         res.status(200).json({
+             status:true,
+             message: "Image uploaded successfully!",
+             data: post
+         })
+
+    } catch (error) {   
+         res.status(400).json({
+            status:false,
+             message :"server error" + error.message
+         })
+    }
+})
 
 // app.use("/", (req, res) => {
 //     res.send("hello from the server")
@@ -79,15 +125,21 @@ app.use(cors())
 
 
 
-app.post("/uploads", upload.fields([{name:"profileImage"},{name:"coverImage"}]),  async (req, res) => {
+// app.post("/uploads", upload.fields([{name:"profileImage"},{name:"coverImage"}]),  async (req, res) => {
    
-    let file = req.file
+//     let file = req.file
 
-    console.log(file)
-    // console.log()
+//     console.log(file)
+//     // console.log()
 
-    res.status(200).json({
-        message:"kaam hogiyaa..."
-    })
+//     res.status(200).json({
+//         message:"kaam hogiyaa..."
+//     })
 
-})
+// })
+
+
+
+
+
+
