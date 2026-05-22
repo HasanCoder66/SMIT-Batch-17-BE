6cae8c2fc1dbfe9e93a0ec4791514681433f01ce
hasanCoder66 <hasanashraf874@gmail.com>
2026-05-22 10:29:21 +0500

    chat app with sockets io, emit, on, socket, connection, disconnect

--- nodejs/code/chat-app/public/app.js
+++ nodejs/code/chat-app/public/app.js
@@ -1,3 +1,61 @@
 const socket = io()
 
 console.log("client connected ==>");
+
+let messageInput = document.getElementById("message-input")
+let sendBtn = document.getElementById("send-btn")
+
+let usernameInput = document.getElementById("username")
+let joinBtn = document.getElementById("join")
+
+let onlineUsers = document.getElementById("onlineUsers")
+
+console.log(messageInput.value);
+console.log(sendBtn);
+
+
+
+
+
+
+
+joinBtn.addEventListener("click",() =>{
+    let userName = usernameInput.value;
+
+    if(!userName) {
+        console.log("please enter a user name");
+        
+        return 
+    }
+
+    socket.emit("joined-user",  userName)
+
+
+     usernameInput.value = ""
+
+
+     socket.on("online-users", (totalUsers) => {
+        console.log("total users ==>",totalUsers);
+        onlineUsers.innerHTML = `Online Users : ${totalUsers}`
+        
+     })
+
+
+
+})
+
+sendBtn.addEventListener("click", () => {
+    const message = messageInput.value;
+
+
+    // console.log("send btn click howaa....", message);
+    socket.emit("message", message)
+
+
+    socket.on("message", (data) =>{
+        console.log("server ne messages bhejhy...",data);
+        
+    })
+    
+    messageInput.value = ""    
+})
--- nodejs/code/chat-app/server.js
+++ nodejs/code/chat-app/server.js
@@ -8,11 +8,44 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
+let users = []
 
 app.use(express.static("public"))
 
 io.on("connection", (socket) => {
     console.log("user :", socket.id);
+
+
+    socket.on("message", (message) =>{
+    console.log("user ny message bhejha hai..", message);
+
+    io.emit("message", message)
+
+    })
+
+
+      socket.on("joined-user", (userName) => {
+        console.log(`${userName} joined chat`);
+
+        users.push({
+            id:socket.id,
+            userName 
+        })
+        
+        console.log(users);
+        
+        io.emit("online-users", users.length)
+    })
+
+    socket.on("disconnect", () => {
+users = users.filter((user) => user.id !== socket.id)
+    io.emit("online-users", users.length)
+    })
+
+
+
+    
+    
     
 })
 
