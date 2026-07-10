eb762f7968e256598ee1559ca18499de62caa239
hasanCoder66 <hasanashraf874@gmail.com>
2026-07-10 10:23:26 +0500

    interfaces and type aliases

---
+++ Typescript/code/interfaces-and-type-aliases/app.ts
@@ -0,0 +1,151 @@
+let a = 22;
+
+console.log(a);
+
+// a = "string...."
+
+
+// Tuples -->
+
+// let myArr = ["one", "two"]
+// let myArr2 = [1,2]
+// let mixedArr = ["1",2]
+
+
+// let myArr:[string, number, boolean] = ["", 34, true];
+
+
+
+// enum Status{
+//     FALSE = "FALSE",
+//     SUCCESS = "SUCCESS"
+// }
+
+
+// enum StatusCode {
+// NOT_FOUND = 404,
+// SUCCESS = 200,
+// RESOURCE_CREATED = 201,
+// }
+
+// StatusCode.RESOURCE_CREATED
+
+
+
+
+
+// let myVariable;
+
+// myVariable = 12
+// myVariable ="janai"
+
+
+// let age;
+// // age = 23
+// age = "23"
+
+// age.to
+
+
+
+// let kuchBi:unknown;
+
+// // type narrowin...
+
+
+// if(typeof kuchBi == "string"){
+//     // kuchBi. 
+// }
+
+// if(typeof kuchBi == "number"){
+//     kuchBi.toFixed()
+// }
+
+
+
+// function sum ():string{
+// return "2 + 5"
+// }
+
+// console.log(sum());
+
+
+// let isAdmin:null = null
+
+// let myVal:undefined = undefined;
+
+
+
+// let isAdmin:boolean | string = true
+
+
+// interface User {
+//     name : string,
+//     password: string,
+// }
+
+
+// function submitHandler (data:User){
+
+//     console.log("form submitted !", data);
+    
+// }
+
+
+// submitHandler({name:"hasan", password:"1234566789"})
+
+
+
+// interface User {
+//     name : string,
+//     password: string,
+// }
+
+
+// interface Admin extends User{
+//     isAdmin : boolean
+// }
+
+
+// function abcd (obj:User){
+// obj.
+// }
+
+
+
+
+// interface abcd {
+//     name: string,
+// }
+
+
+// interface abcd {
+//     age: string,
+//     gender?: string
+// }
+
+// function abcde (obj:abcd){
+//     obj.
+// }
+
+// abcde({
+//     name:"",
+//     age:"",
+//     // gender:""
+// })
+
+// type value =string | number | boolean | null
+
+
+// let val:value = 12;
+
+type user ={
+    name: string
+}
+
+type admin = string & user
+
+
+// let val:admin = "janiii" | {
+//     name: "stirnng"
+// };
