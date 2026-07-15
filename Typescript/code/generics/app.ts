2a910e8e3fe188a147aaaa804d3aef01bf2beaf9
hasanCoder66 <hasanashraf874@gmail.com>
2026-07-15 09:44:07 +0500

    generics functions or interfaces

---
+++ Typescript/code/generics/app.ts
@@ -0,0 +1,40 @@
+//  Generics 
+
+// Generic Function 
+// Generic Interface
+
+
+
+// function abc<T>(a:T, b:number, c:boolean){
+
+// }
+
+// abc("",12,false )
+// abc(12, 45, true)
+// abc(12)
+// abc(true)
+
+
+// interface User<T>{
+//     email : string,
+//     password  : string,
+//     key : T
+// }
+
+
+// function foo(para: User<string>):void{
+
+// }
+
+// foo({
+//     email : "",
+//     password : "",
+//     key: "12345"
+// })
+
+
+// function random<T>(a:T):T{
+// return "" as T
+// }
+
+// random("hasan")
