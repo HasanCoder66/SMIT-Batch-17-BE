d78c3e44c4e7f71764d8bad0aeda61fa5a5003d9
hasanCoder66 <hasanashraf874@gmail.com>
2026-07-24 10:27:03 +0500

    redis done

--- nodejs/code/scratch/app.js
+++ nodejs/code/scratch/app.js
@@ -4,9 +4,16 @@ import helmet from "helmet"
 import { rateLimit } from 'express-rate-limit'
 import authRoutes from "./src/routes/authRoutes.js"
 import { connectDb } from "./src/db/db.js"
-import userRoutes from "./src/routes/userRoute.js"
+// import userRoutes from "./src/routes/userRoute.js"
 import { errorMiddleware } from "./src/middleware/errorMiddleware.js"
-import jobRoute from "./src/routes/jobRoutes.js"
+import { User } from "./src/models/user.js"
+// import jobRoute from "./src/routes/jobRoutes.js"
+import { createClient } from "redis";
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
 
 
 const app = express()
@@ -21,6 +28,20 @@ const limiter = rateLimit({
 	
 })
 
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+});
+
+redisClient.on("error", (err) => {
+    console.error("Redis Error:", err);
+});
+
+(async () => {
+    await redisClient.connect();
+    console.log("redis connected!");
+    
+})();
+
 // Apply the rate limiting middleware to all requests.
 app.use(limiter)
 
@@ -32,9 +53,51 @@ app.get("/",(req, res) => {
 })
 
 
+
+
 app.use("/api/auth", authRoutes)
-app.use("/api/user", userRoutes)
-app.use("/api/job", jobRoute)
+
+app.put("/api/users", (req, res) => {
+    redisClient.del("users")
+
+    return res.status(200).json({
+        status:true,
+        message:"user updated successfully!"
+    }
+
+    )
+})
+app.get("/api/users", async (req, res) => {
+
+    const cacheKey = `users`;
+
+    const redisData = await redisClient.get(cacheKey);
+
+    if(!redisData){
+        const users = await User.find()
+
+        redisClient.set(cacheKey, JSON.stringify(users), { EX: 864000 })
+
+    return res.status(200).json({
+        status:true,
+        data: users,
+        message:"users data agiya..."
+    })
+
+    }else{
+        return res.status(200).json({
+            status: true,
+            message:"data leaya hon..",
+            data : JSON.parse(redisData)
+        })
+    }
+
+    console.log(users);
+    
+   
+})
+// app.use("/api/user", userRoutes)
+// app.use("/api/job", jobRoute)
 // app.use("/api/user")
 
 
